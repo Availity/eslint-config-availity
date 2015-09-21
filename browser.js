9bfac32c6ad89d09b283590d3c50da932a0a17e5
Rob McGuinness <rob.mcguinness@availity.com>
2015-09-20 21:31:37 -0400

    add browser config

---
+++ browser.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "/index",
+  "env": {
+    "browser": true,
+    "commonjs": true
+  },
+  "rules": {
+    "strict": [
+      2,
+      "global"
+    ],
+    "global-strict": 0
+  }
+}
