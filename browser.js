dcdedc8829107201c9f188094b9dcb81bbfb7e99
Rob McGuinness <rob.mcguinness@availity.com>
2015-11-30 23:24:08 -0500

    browser configuration

--- browser.js
+++ browser.js
@@ -1,16 +1,7 @@
 module.exports = {
-
   'extends': './index',
   'env': {
     'browser': true,
     'commonjs': true
-  },
-  'rules': {
-    'strict': [
-      2,
-      'global'
-    ],
-    'global-strict': 0
   }
-
 }
