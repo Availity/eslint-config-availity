b14f5a533972718c56eac944cf7faa4a9ce6a941
Rob McGuinness <rob.mcguinness@availity.com>
2017-02-09 14:44:40 -0500

    refactor

--- browser.js
+++ browser.js
@@ -1,25 +1,22 @@
 // Target is browser code written in ES6 using a transpiler like Babel
 module.exports = {
 
-  'extends': './base.js',
+  "extends": "./base.js",
 
-  'env': {
-    'browser': true,
-    'es6': true
+  "env": {
+    "browser": true,
+    "es6": true
   },
 
   // Force ES6 module settings: http://www.2ality.com/2014/09/es6-modules-final.html
-  'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'modules': false
-    }
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
   },
 
   "globals": {
-    "document": false,
-    "navigator": false,
-    "window": false
+    "document": true,
+    "navigator": true,
+    "window": true
   }
 }
