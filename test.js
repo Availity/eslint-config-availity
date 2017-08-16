22880d04ba602270e133cbcad80c45244c9cd2a2
Rob McGuinness <rob.mcguinness@availity.com>
2017-08-16 19:47:21 -0400

    add test for no-used (static)

--- test.js
+++ test.js
@@ -15,14 +15,36 @@ function runEslint(str, conf) {
 
 }
 
-test('rules', (t) => {
-
-  t.plan(4);
-
+test('config', (t) => {
+  t.plan(2);
   t.ok(isPlainObj(conf));
   t.ok(isPlainObj(conf.rules));
+});
 
-  const errors = runEslint(`'use strict'\nvar foo = function () {};\nfoo();\n`, conf);
+test('semi, no-var', (t) => {
+  t.plan(2);
+
+  const errors = runEslint(`
+'use strict'
+var foo = function () {};
+foo();
+`, conf);
+
   t.equal(errors[0].ruleId, 'semi');
   t.equal(errors[1].ruleId, 'no-var');
 });
+
+test('no-used (static)', (t) => {
+  t.plan(1);
+
+  const errors = runEslint(`
+'use strict';
+class Service {
+  static $inject = ['$scope'];
+}
+Service.$inject.length;
+` , conf);
+
+  t.equal(errors.length, 0);
+})
+
