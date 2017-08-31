e6a6a0704170aea8f88c5e599d77cab9e31224f5
Rob McGuinness <rob.mcguinness@availity.com>
2017-08-31 09:54:11 -0400

    don't enforce jsx extension

--- browser.js
+++ browser.js
@@ -28,6 +28,9 @@ module.exports = {
       '__STAGING__': true
     },
 
-    'rules': Object.assign(base.rules, { 'react/sort-comp': 0})
+    'rules': Object.assign(base.rules, {
+      'react/sort-comp': 0,
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    })
 
   };
