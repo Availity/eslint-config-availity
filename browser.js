6a8eaffdc45625111c56d316e073513f9e9eebbd
Rob McGuinness <rob.mcguinness@availity.com>
2017-08-30 23:45:13 -0400

    closes #5: convert to airbnb/javascript

--- browser.js
+++ browser.js
@@ -1,19 +1,33 @@
 // Target is browser code written in ES6 using a transpiler like Babel
+const base = require('./base');
+
 module.exports = {
 
-  "extends": "./base.js",
+    'extends': [
+      'airbnb',
+      'plugin:promise/recommended'
+    ],
 
-  "parserOptions": {
-    "sourceType": "module"
-  },
+    "plugins": [
+      "promise"
+    ],
 
-  "globals": {
-    "document": true,
-    "navigator": true,
-    "window": true,
-    "__DEV__": true,
-    "__TEST__": true,
-    "__PROD__": true,
-    "__STAGING__": true
-  }
-}
+    'parser': 'babel-eslint',
+
+    'parserOptions': {
+      'sourceType': 'module'
+    },
+
+    'globals': {
+      'document': true,
+      'navigator': true,
+      'window': true,
+      '__DEV__': true,
+      '__TEST__': true,
+      '__PROD__': true,
+      '__STAGING__': true
+    },
+
+    'rules': Object.assign(base.rules, { 'react/sort-comp': 0})
+
+  };
--- test.js
+++ test.js
@@ -3,37 +3,59 @@ const isPlainObj = require('is-plain-obj');
 const tempWrite = require('temp-write');
 const eslint = require('eslint');
 const conf = require('./');
+const reactConf = require('./browser')
 
-function runEslint(str, conf) {
+function runEslint(str, configuration) {
 
   const linter = new eslint.CLIEngine({
     useEslintrc: false,
-    configFile: tempWrite.sync(JSON.stringify(conf))
+    configFile: tempWrite.sync(JSON.stringify(configuration))
   });
 
   return linter.executeOnText(str).results[0].messages;
 
 }
 
-test('', (t) => {
+test('base rules', (t) => {
 
-  t.plan(5);
+  t.plan(6);
 
   t.ok(isPlainObj(conf), 'should exist');
-  t.ok(isPlainObj(conf.rules, ), 'should have rules configuration');
+  t.ok(isPlainObj(conf.rules), 'should have rules configuration');
 
-  const errors = runEslint(`var foo = function () {};
+  const errors = runEslint(`var foo = function foo() {};
 foo()
 const arr = [
   1,
   2
 ];
-var a = 3;
-function b() {
-  var a = 10;
-}
 `, conf);
+
   t.equal(errors[0].ruleId, 'no-var', 'should error when using var');
   t.equal(errors[1].ruleId, 'semi', 'should error when missing semi-colons');
+  t.equal(errors[2].ruleId, 'no-unused-vars', 'should error variables not used');
   t.equal(errors[3].ruleId, 'comma-dangle', 'should error when not using commas in multie-line arays');
+});
+
+test('react rules', (t) => {
+
+  t.plan(3);
+
+  t.ok(isPlainObj(reactConf), 'should exist');
+  t.ok(isPlainObj(reactConf.rules), 'should have rules configuration');
+
+  const errors = runEslint(`import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+
+@observer
+export default class Patient extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+`, reactConf);
+
+  t.equal(errors[0].ruleId, 'import/no-extraneous-dependencies', 'should error modules not declare in package.json');
 });
