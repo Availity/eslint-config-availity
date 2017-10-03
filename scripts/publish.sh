50e4362272de29b71d45d61079d7a7f6962000ca
Rob McGuinness <rob.mcguinness@availity.com>
2017-10-03 13:35:48 -0400

    chore(build): git commit with annotated tags on release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -21,3 +21,4 @@ echo "==> Updating Changelog"
 node_modules/.bin/conventional-changelog -i CHANGELOG.md -o CHANGELOG.md -p angular
 git add .
 git commit -m "chore(release): ${VERSION}"
+git tag -a v${VERSION} -m "chore(release): ${VERSION}"
