a4bf2fd48d401701120b717fb194b54000868c80
Rob McGuinness <rob.mcguinness@availity.com>
2017-12-08 10:57:58 -0500

    test(prettier): remove semi test condition

--- test.js
+++ test.js
@@ -14,33 +14,36 @@ function runEslint(str, configuration) {
   return linter.executeOnText(str).results[0].messages;
 }
 
-test('base rules', (t) => {
+test('base rules', t => {
   t.plan(6);
 
   t.ok(isPlainObj(conf), 'should exist');
   t.ok(isPlainObj(conf.rules), 'should have rules configuration');
 
-  const errors = runEslint(`var foo = function foo() {};
+  const errors = runEslint(
+    `var foo = function foo() {};
 foo()
 const arr = [
   1,
   2
 ];
-`, conf);
+`,
+    conf
+  );
 
   t.equal(errors[0].ruleId, 'no-var', 'should error when using var');
-  t.equal(errors[1].ruleId, 'semi', 'should error when missing semi-colons');
   t.equal(errors[2].ruleId, 'no-unused-vars', 'should error variables not used');
   t.equal(errors[3].ruleId, 'comma-dangle', 'should error when not using commas in multie-line arays');
 });
 
-test('react rules', (t) => {
+test('react rules', t => {
   t.plan(3);
 
   t.ok(isPlainObj(reactConf), 'should exist');
   t.ok(isPlainObj(reactConf.rules), 'should have rules configuration');
 
-  const errors = runEslint(`import React, { Component } from 'react';
+  const errors = runEslint(
+    `import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 
 @observer
@@ -51,7 +54,9 @@ export default class Patient extends Component {
     );
   }
 }
-`, reactConf);
+`,
+    reactConf
+  );
 
   t.equal(errors[0].ruleId, 'import/no-extraneous-dependencies', 'should error modules not declare in package.json');
 });
