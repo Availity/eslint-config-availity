8ed5efb749f12a2f1689d759b85e61f85ea98184
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2019-01-13 12:34:39 -0500

    refactor: enable more recommended settings
    Also beef up unit tests to validate the overriden rules

--- base.js
+++ base.js
@@ -1,9 +1,9 @@
 module.exports = {
   parser: 'babel-eslint',
 
-  extends: ['airbnb-base', 'plugin:promise/recommended', 'prettier'],
+  extends: ['airbnb-base', 'plugin:promise/recommended', 'plugin:jest/recommended', 'prettier'],
 
-  plugins: ['promise'],
+  plugins: ['promise', 'jest'],
 
   parserOptions: {
     sourceType: 'script',
@@ -17,15 +17,11 @@ module.exports = {
     strict: 0,
     'no-var': 2,
     'no-shadow': 0,
-    'arrow-body-style': 2,
     'no-param-reassign': 0,
-    'consistent-return': 0,
     'arrow-parens': ['error', 'as-needed'],
     'class-methods-use-this': 0,
     'import/no-extraneous-dependencies': 0,
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'object-curly-newline': 0,
-    'function-paren-newline': 0,
     'no-underscore-dangle': 0,
     'promise/avoid-new': 0,
   },
--- test.js
+++ test.js
@@ -14,45 +14,116 @@ function runEslint(str, configuration) {
   return linter.executeOnText(str).results[0].messages;
 }
 
-test('base rules', () => {
-  expect(isPlainObj(conf)).toBeTruthy();
-  expect(isPlainObj(conf.rules)).toBeDefined();
+describe('rules', () => {
+  test('base', () => {
+    expect(isPlainObj(conf)).toBeTruthy();
+    expect(isPlainObj(conf.rules)).toBeDefined();
 
-  const errors = runEslint(
-    `var foo = function foo() {};
+    const errors = runEslint(
+      `
+// no-var
+var foo = function foo() {};
+
 foo()
+
+// no-unused-vars
 const arr = [
   1,
   2
 ];
-`,
-    conf
-  );
 
-  expect(errors[0].ruleId).toBe('no-var');
-  expect(errors[1].ruleId).toBe('no-unused-vars');
-});
+// no-param-reassign
+function foo(bar) {
+    bar = 13;
+}
 
-test('react rules', () => {
-  expect(isPlainObj(reactConf)).toBeDefined();
-  expect(isPlainObj(reactConf.rules)).toBeDefined();
+// no-shadow
+var a = 3;
+function b() {
+    var a = 10;
+}
 
-  const errors = runEslint(
-    `import React, { Component } from 'react';
+// class-methods-use-this
+class A {
+    foo() {
+        console.log("Hello World");
+    }
+}
 
-export default class Patient extends Component {
-  render() {
-    return (
-      <div>Hello</div>
-    );
-  }
+// no-plusplus
+const l = 10;
+for (i = 0; i < l; i++) {
+    return;
+}
 
-  displayName : 'Hello'
+// no-underscore-dangle
+foo._bar();
+
+// promise/avoid-new
+const fn = () =>  {
+  return new Promise((resolve, reject) => {
+    return resolve(true);
+  });
 }
 `,
-    reactConf
-  );
+      conf
+    );
 
-  const result = find(errors, { ruleId: 'react/sort-comp' });
-  expect(result).toBeUndefined();
+    // Enabled
+    expect(find(errors, { ruleId: 'no-var' })).toBeDefined();
+    expect(find(errors, { ruleId: 'no-unused-vars' })).toBeDefined();
+
+    // Disabled
+    expect(find(errors, { ruleId: 'no-param-reassign/sort-comp' })).toBeUndefined();
+    expect(find(errors, { ruleId: 'class-methods-use-this' })).toBeUndefined();
+    expect(find(errors, { ruleId: 'no-plusplus' })).toBeUndefined();
+    expect(find(errors, { ruleId: 'no-underscore-dangle' })).toBeUndefined();
+    expect(find(errors, { ruleId: 'promise/avoid-new' })).toBeUndefined();
+  });
+
+  test('react', () => {
+    expect(isPlainObj(reactConf)).toBeDefined();
+    expect(isPlainObj(reactConf.rules)).toBeDefined();
+
+    const errors = runEslint(
+      `
+import React, { Component } from 'react';
+import { Link } from '@reach/router';
+
+class Patient extends Component {
+  render({ a }) {
+    return (
+      <>
+        <div>Hello {a}!</div>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+  }
+  displayName : 'Hello'
+}
+
+// react/require-default-props
+Patient.propTypes = {
+  a: PropTypes.string,
+  b: PropTypes.array,
+  c: PropTypes.object,
+  d: PropTypes.any
+};
+
+export default Patient;
+
+`,
+      reactConf
+    );
+
+    // Enabled
+    expect(find(errors, { ruleId: 'react/forbid-prop-types' })).toBeDefined();
+
+    // Disabled
+    expect(find(errors, { ruleId: 'react/sort-comp' })).toBeUndefined();
+    expect(find(errors, { ruleId: 'react/require-default-props' })).toBeUndefined();
+    expect(find(errors, { ruleId: 'jsx-a11y/anchor-is-valid' })).toBeUndefined();
+  });
 });
