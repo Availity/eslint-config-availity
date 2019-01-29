345df16c5cb1430128784a8b299f039300bf2b3f
Kyle <kgray1497@gmail.com>
2019-01-29 10:06:47 -0500

    refactor(eslint): disabled filename-case from unicorn

--- base.js
+++ base.js
@@ -3,7 +3,7 @@ module.exports = {
 
   extends: ['airbnb-base', 'plugin:promise/recommended', 'plugin:jest/recommended', 'plugin:unicorn/recommended', 'prettier'],
 
-  plugins: ['promise', 'jest','unicorn'],
+  plugins: ['promise', 'jest', 'unicorn'],
 
   parserOptions: {
     sourceType: 'script',
@@ -20,7 +20,8 @@ module.exports = {
     'no-param-reassign': 0,
     'arrow-parens': ['error', 'as-needed'],
     'class-methods-use-this': 0,
-    "unicorn/import-index":"off",
+    "unicorn/import-index": "off",
+    "unicorn/filename-case": "off",
     'import/no-extraneous-dependencies': 0,
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'no-underscore-dangle': 0,
