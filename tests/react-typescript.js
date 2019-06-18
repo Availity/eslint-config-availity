1f63b1d442ad257a5bb68cbc13edd827ec2413fe
Kyle Gray <Kyle.Gray@availity.com>
2019-06-18 15:59:00 -0400

    refactor: adding unit test for disabling ts linter rule

--- tests/react-typescript.js
+++ tests/react-typescript.js
@@ -4,4 +4,7 @@ import * as React from 'react';
 export interface HelloProps { compiler: String; framework: string; }
 
 export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+
+// testing no-unused-vars
+const hello: String  = "World";
 `;
