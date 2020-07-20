2e0d39764d7ab2e811f3039f8b1ed95af53b5692
Rob McGuinness <rob.mcguinness@availity.com>
2020-07-19 21:14:35 -0400

    feat: make @typescript-eslint parser optional

--- base.js
+++ base.js
@@ -1,3 +1,32 @@
+const resolve = require('resolve');
+const path = require('path');
+const fs = require('fs');
+
+const overrides = [];
+
+// Lint tsx only if typescript is installed
+try {
+  resolve.sync('typescript', {
+    basedir: path.resolve(fs.realpathSync(process.cwd()), 'node_modules'),
+  });
+
+  overrides.push({
+    files: ['*.{ts,tsx}'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      warnOnUnsupportedTypeScriptVersion: true,
+    },
+    extends: ['plugin:@typescript-eslint/recommended'],
+    plugins: ['@typescript-eslint'],
+  });
+} catch (error) {
+  if (error.code !== 'MODULE_NOT_FOUND') {
+    throw error;
+  }
+}
+
 module.exports = {
   root: true,
 
@@ -21,19 +50,7 @@ module.exports = {
     jest: true,
   },
 
-  overrides: [
-    {
-      files: ['*.{ts,tsx}'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        warnOnUnsupportedTypeScriptVersion: true,
-      },
-      extends: ['plugin:@typescript-eslint/recommended'],
-      plugins: ['@typescript-eslint'],
-    },
-  ],
+  overrides,
 
   rules: {
     strict: 0,
