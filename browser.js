6d7d41fce049b44bb8fd1bbeffc930fc68a5d81e
Jordan Young <jordan.young@availity.com>
2024-05-07 16:05:58 -0400

    feat: update to eslint 8 and update dependencies
    BREAKING CHANGE: Node ^18.18.0 or ^20 is required

--- browser.js
+++ browser.js
@@ -63,6 +63,7 @@ module.exports = {
 
   overrides: [
     ...base.overrides,
+    // Disable prop-types rule in .tsx files
     {
       files: ['**/*.tsx'],
       rules: {
@@ -73,14 +74,18 @@ module.exports = {
 
   rules: {
     ...base.rules,
-    'unicorn/no-for-loop': 0,
+    // Only warn when arrow function can use implicit return
+    'arrow-body-style': ['warn', 'as-needed'],
     'unicorn/prefer-query-selector': 0,
+    // Disable requiring class components to be created with same order
     'react/sort-comp': 0,
     // Replace Airbnb 'camelcase' rule with '@typescript-eslint/naming-convention'
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
     camelcase: 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    // Disable requiring every optional prop to have a default value
     'react/require-default-props': 0,
+    // Check labels have an id to associate with an input
     'jsx-a11y/label-has-associated-control': [
       2,
       {
@@ -89,15 +94,23 @@ module.exports = {
         },
       },
     ],
-    'react-hooks/rules-of-hooks': 'warn',
+    // Error when rules of hooks are broken
+    'react-hooks/rules-of-hooks': 'error',
+    // Warn when not all deps are in dep array
     'react-hooks/exhaustive-deps': 'warn',
+    // Disable props not allowed to be spread
     'react/jsx-props-no-spreading': 0,
+    // Error when 'any' prop-type is used
     'react/forbid-prop-types': [
       'error',
       {
         forbid: ['any'],
       },
     ],
+    // Disable requiring React to be imported. Not needed with v17+ of React
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    // Make href required except when using Link component 'to' is accepted
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
@@ -105,6 +118,7 @@ module.exports = {
         specialLink: ['to'],
       },
     ],
+    // Do not require file extensions
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -114,6 +128,15 @@ module.exports = {
         jsx: 'never',
         ts: 'never',
         tsx: 'never',
+      },
+    ],
+    // Disabled. default export is not required
+    'import/prefer-default-export': 0,
+    // Allow component to be standard function or arrow function
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: ['function-declaration', 'arrow-function'],
       },
     ],
     // https://github.com/typescript-eslint/typescript-eslint/issues/2540
